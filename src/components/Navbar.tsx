c4f38bafc84662cee24393d61475d55bae106f67
gumartins77 <gugumartin77@gmail.com>
2023-08-12 16:48:50 -0300

    feat: add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { FC } from 'react'
+
+const Navbar: FC = () => {
+  return (
+    <header>
+      <div className="navbar bg-base-100 container">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost normal-case text-xl">
+            Marvel Comics
+          </Link>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
--- src/components/Test.tsx
+++
@@ -1,7 +0,0 @@
-export function TestComponent() {
-  return (
-    <div>
-      <h2>Componente teste</h2>
-    </div>
-  )
-}
