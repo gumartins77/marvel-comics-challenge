048653cac461160e440f54673011f46a87140500
gumartins77 <gugumartin77@gmail.com>
2023-08-12 16:49:34 -0300

    feat: add page of all characters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { TestComponent } from '@/components/Test'
+import CharacterCard from '@/Components/CharacterCard'
+import { getCharacters } from '@/utils/api'
 
-export default function Home() {
+export default async function Home() {
+  const characters = await getCharacters()
+
   return (
     <main>
-      <h1>Hello world</h1>
-      <TestComponent />
+      <div className="container text-center mt-10">
+        <h1 className="text-3xl font-bold underline">Characters Marvels</h1>
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 mt-10">
+          {characters.results.map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
