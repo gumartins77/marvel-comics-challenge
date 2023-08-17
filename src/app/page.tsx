42ba4438ba25e6543f445055a7b85897907d73a8
gumartins77 <gugumartin77@gmail.com>
2023-08-17 10:43:38 -0300

    feat: implement image in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { FC } from 'react'
 const Home: FC = () => {
   return (
     <div className="card w-full bg-base-200 shadow-xl">
-      <figure>
+      <figure style={{ width: '100vw', height: '100vh', margin: 0 }}>
         <Image
           src={`/img/marvel.jpg`}
           alt="Marvel-Logo"
-          width={500}
-          height={400}
+          layout="fill"
+          objectFit="cover"
         />
       </figure>
     </div>
