f10ea9d6a920f7eb03b027f3f4e2a8fa97b6f8a3
gumartins77 <gugumartin77@gmail.com>
2023-08-18 11:31:59 -0300

    feat: responsive homepage image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,23 @@ import { FC } from 'react'
 
 const Home: FC = () => {
   return (
-    <>
+    <div className="flex items-center justify-center h-screen">
       <head>
         <title>Home</title>
       </head>
       <div className="card w-full bg-base-200 shadow-xl">
-        <figure style={{ width: '100vw', height: '100vh', margin: 0 }}>
+        <figure style={{ width: '100%', height: '100%' }}>
           <Image
-            src={`/img/marvel.jpg`}
+            src="/img/marvel.jpg"
             alt="Marvel-Logo"
-            layout="fill"
+            layout="responsive"
             objectFit="cover"
+            width={1920}
+            height={1080}
           />
         </figure>
       </div>
-    </>
+    </div>
   )
 }
 
